ccf02f5d07b5d4d63fd9822d0b7bd85d7c7c08df
SebasDiez84 <49246065+SebasDiez84@users.noreply.github.com>
2020-09-29 23:47:50 -0500

    back e intento de coneccion

--- Frontend/Js/Pedidos.js
+++ Frontend/Js/Pedidos.js
@@ -6,6 +6,8 @@ export default{
     },
     data(){
         return {
+            pedidos: [],
+
             Pedido:{
                 CodigoPedidoDB: "",
                 CodigoPedido: "",
@@ -37,7 +39,61 @@ export default{
                 acciones: true
             }
             return this.formularioPedido
-        }
+        },
+        //Carga la lista de las aplicaciones desde la base de datos
+        async cargarPedidos() {
+            let url = 'https://api-pedidos-software3.herokuapp.com/pedidos';
+            this.loading = true;
+            //Trae todos los marcadores desde la base de datos.
+            this.$axios
+            .get(url)
+            .then(response => {
+            //Asigna los marcadores al array de marcadores global para ser enlistados.
+            this.pedidos = response.data.info;
+            this.ordenarAsc(this.pedidos, 'CodigoPedido');
+  
+        })
+            .catch(error => {
+            console.log(error);
+        });
+  
+        },
+        //Guarda la aplicación que fue ingresada en la página.
+        guardarRegistro() {
+        let url = 'https://api-pedidos-software3.herokuapp.com/pedidos'
+        this.$axios.post(url, this.datosFormulario()).then(respuesta => {
+          //Recarga los marcadores de la base de datos.
+          this.$swal.fire({
+            title: "Registro guardado",
+            text: "El registro fue guardado con exito",
+            type: "success",
+            timer: 3000
+          }).then(r => {
+            window.open('https://api-pedidos-software3.herokuapp.com/pedidos', '_self');
+          });
+          this.cargarLista();
+          this.limpiarLista();
+  
+  
+        });
+        },
+         //Limpia los campos del formulario.
+        limpiarLista() {
+        this.form.inputCodigoPedido = '';
+        this.form.inputCodigoProducto = '';
+        this.form.inputCantidad = '';
+        this.form.inputValor = '';
+        this.form.inputFecha = '';
+        this.refrescarFormulario();
+  
+      },
+      refrescarFormulario() {
+        this.show = false;
+        this.$nextTick(() => {
+          this.show = true;
+        });
+  
+      },
     }
 
 };
